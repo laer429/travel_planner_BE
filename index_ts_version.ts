ce050ca4cbd8eb9aba1f706f81ef18c1996f04a1
laer429 <laer429@gmail.com>
2023-04-11 21:43:43 +0900

    ts_update

--- index_ts_version.ts
+++ index_ts_version.ts
@@ -1,16 +1,20 @@
-const express = require('express');
-const app = express();
-const mysql = require('mysql');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const port = 3000;
+import * as dotenv from "dotenv";
+import mysql from 'mysql';
+import cors from 'cors';
+import express, {Express, Request, Response} from 'express';
+import bodyParser from 'body-parser';
 
-require('dotenv').config();
+const app: Express = express();
+dotenv.config();
+
+const port: number = 3000;
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cors());
-
-const conn = {
+const conn : {
+    [key :string] : string,
+} = {
   host : process.env.db_host,
   port : process.env.db_port,
   user : process.env.db_user,
@@ -21,38 +25,44 @@ const conn = {
 const connection = mysql.createConnection(conn);
 
 connection.connect();
+console.log('db success, port:'+port);
+
+app.get('/', () => {
+    return 'hello world';
+});
 
 
-//내 일정 부분에 일정 보여주기
-app.get('/location', (req,res) => {
+// 내 일정 부분에 일정 보여주기
+app.get('/location', (req: Request, res: Response) => {
   connection.query('select id, turn, location_name, address, mapx, mapy from travel_planner.location order by turn',function(error,results,fields) {
     res.send(results);
     if (error) {
       console.log('db connection error');
-      res.statusCode(500);
+      res.status(500);
+      res.end();
     }
   });
 });
 
 //search_api 에서 찾은 결과를 내 일정에 추가하기
-app.post('/location', (req,res) => {
-  const input = req.body;
-  if(!input.turn || !input.location_name || !input.address || !input.mapx || !input.mapy) {
+app.post('/location', (req: Request, res: Response) => {
+  if(!req.body.turn || !req.body.location_name || !req.body.address || !req.body.mapx || !req.body.mapy) {
     res.status(400);
     res.end();
   } else {
-    let datas = [
-      input.turn,
-      input.location_name,
-      input.address,
-      input.mapx,
-      input.mapy
+    let datas :(number|string)[] = [
+      req.body.turn,
+      req.body.location_name,
+      req.body.address,
+      req.body.mapx,
+      req.body.mapy
   ];
   connection.query("insert into location values(null,?,?,?,?,?,now(),now())",datas, function (error, results, fields) {
-    if (error) {
-      console.log('db connection error');
-      res.statusCode(500);
-    }
+      if (error) {
+        console.log('db connection error');
+        res.status(500);
+        res.end();
+      }
     res.status(201);
     res.end();
   });
@@ -60,30 +70,31 @@ app.post('/location', (req,res) => {
 });
 
 //일정 삭제
-app.delete('/location:id',(req,res) => {
+app.delete('/location:id',(req: Request, res: Response) => {
   if (!req.params.id) {
     res.status(400);
     res.end();
   } else {
-    let id = req.params.id;
-    connection.query("delete from travel_planner.location where id = ?;",id, function (error, results, fields) {
-      if (error) {
-        console.log('db connection error');
-        res.statusCode(500);
-      }
-      res.status(200);
+    let id : string = req.params.id;
+  connection.query("delete from travel_planner.location where id = ?;",id, function (error, results, fields) {
+    res.status(200);
+    res.end();
+    if (error) {
+      console.log('db connection error');
+      res.status(500);
       res.end();
-    });
+    }
+  });
   }
 });
 
 //일정 순서 바꾸기
-app.put('/location', (req,res) => {
-  let fst_datas = [
+app.put('/location', (req: Request, res: Response) => {
+  let fst_datas : string[] = [
       req.body.fst_turn,
       req.body.fst_id
   ];
-  let snd_datas = [
+  let snd_datas : string[] = [
     req.body.snd_turn,
     req.body.snd_id
   ]
@@ -101,10 +112,10 @@ app.put('/location', (req,res) => {
       }
       res.status(200);
       res.end();
+    });
   });
-  });
-  
 });
+  
 
 app.listen(port, '0.0.0.0', async () => {
   console.log('=========================================');
